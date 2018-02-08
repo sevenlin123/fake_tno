4cd7b1fba0afc66e9cc89f3e70dc34b3ae056549
Edward Lin <hsingwel@umich.edu>
2018-02-08 16:44:53 -0500

    Update

--- plutino.py
+++ plutino.py
@@ -6,6 +6,7 @@ planets = load('de423.bsp')
 class plutino:
     """
     generate plutinos
+    units: au, radius
     
     """
     def __init__(self):
@@ -19,6 +20,7 @@ class plutino:
         self.M = self.gen_M()
         self.node = self.gen_node()
         self.arg = self.gen_arg(self.phi, self.M, self.node, self.lambda_N)
+        self.X, self.Y, self.Z = map(self.kep_to_xyz, self.a, self.e, self.i, self.arg, self.node, self.M)
     
     def gen_a(self):
         return 39.45 + np.random.random(self.size) * 0.4 - 0.2
@@ -27,13 +29,13 @@ class plutino:
         return np.random.normal(loc=0.175, scale=0.06, size = self.size)
         
     def gen_i(self):
-        return np.arcsin(np.random.rayleigh(scale = 12 * np.pi/180., size = self.size))*180/np.pi
+        return np.arcsin(np.random.rayleigh(scale = 12 * np.pi/180., size = self.size))
         
     def gen_amp(self):
-        return np.random.triangular(0, 75, 155, size = self.size)
+        return np.random.triangular(0, 75, 155, size = self.size) * np.pi / 180.
     
     def gen_phi(self, amp):
-        return 180 + amp * np.sin(2*np.pi*np.random.random(self.size))
+        return np.pi + amp * np.sin(2*np.pi*np.random.random(self.size))
         
     def gen_M(self):
         return 2*np.pi*np.random.random(self.size)
@@ -52,14 +54,24 @@ class plutino:
         h1s10 = 10**(alpha*h1)
         return np.log10(np.random.random(self.size)*(h1s10-h0s10) + h0s10) / alpha
         
-    def kep_to_xyz(a, e, i, arg, node, M):
+    def kep_to_xyz(self, a, e, i, arg, node, M):
         # compute eccentric anomaly
-        f = lambda E, M, e: E-e*np.sin(E)-M
+        f = lambda E, M, e: E - e * np.sin(E) - M
         E0 = M
         E = newton(f, E0, args=(M, e))
-        # compute eccentric anomaly
-        v = np.arctan2(((1 + e)/(1 - e))**0.5 * np.tan(E/2.))
+        # compute true anomaly
+        v = 2 * np.arctan2((1 + e)**0.5*np.sin(E/2.), (1 - e)**0.5*np.cos(E/2.))
+        # compute the radius
+        r = a * (1 - e*np.cos(E))
+        # compute X,Y,Z
+        X = r * (np.cos(node) * np.cos(arg + v) - np.sin(node) * np.sin(arg + v) * np.cos(i))
+        Y = r * (np.sin(node) * np.cos(arg + v) + np.cos(node) * np.sin(arg + v) * np.cos(i))
+        Z = r * (np.sin(i) * np.sin(arg + v))
+        return X, Y, Z
         
+    def xyz_to_equa(self, X, Y, Z):
+        dec = np.arcsin(Z/(X**2+Y**2+Z**2)**0.5)
+        ra = np.arctan2(Y, X)
         
 def main():
     p = plutino()
