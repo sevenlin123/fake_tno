bdb76e3fe2e7f732311df6e8b18ec3f5271acb6d
Edward Lin <hsingwel@umich.edu>
2018-02-20 17:35:26 -0500

    Add trojan and twotino modes

--- tno_models.py
+++ tno_models.py
@@ -3,49 +3,17 @@ from skyfield.api import load, Topos
 from scipy.optimize import newton
 planets = load('de423.bsp')
 
-class plutino:
+
+class Resonant:
     """
-    generate plutinos
+    The resonant class
     units: au, radius
     
     """
-    def __init__(self, size = 1000, mjd = 57023, e_c = 0.175, e_sigma = 0.06, i_sigma = 12,\
-                 amp_c = 75, amp_max = 155, amp_min = 0):
-        self.size = size
-        self.mjd = mjd
+    def __init__(self):
+        self.size = None
+        self.mjd = None
         self.lambda_N = None
-        self.neptune_lambda()
-        self.a = self.gen_a()
-        self.e = self.gen_e(e_c, e_sigma)
-        self.i = self.gen_i(i_sigma)
-        self.amp = self.gen_amp(amp_c, amp_max, amp_min)
-        self.phi = self.gen_phi(self.amp)
-        self.M = self.gen_M()
-        self.node = self.gen_node()
-        self.arg = self.gen_arg()
-        self.H = self.gen_H()
-        cut = (self.e > 0) * (~np.isnan(self.i))
-        self.a = self.a[cut]
-        self.e = self.e[cut]
-        self.i = self.i[cut]
-        self.amp = self.amp[cut]
-        self.phi = self.phi[cut]
-        self.M = self.M[cut]
-        self.node = self.node[cut]
-        self.arg = self.arg[cut]
-        self.H = self.H[cut]
-        X, Y, Z, r = zip(*map(self.kep_to_xyz, self.a, self.e, self.i,\
-                                                  self.arg, self.node, self.M)) # r**2 = X**2 + Y**2 + Z**2
-        self.X = np.array(X)
-        self.Y = np.array(Y)
-        self.Z = np.array(Z)
-        self.r = np.array(r)
-        self.xyz_to_equa(self.X, self.Y, self.Z)
-        self.H_to_mag()
-        
-    
-    def gen_a(self):
-        return 39.45 + np.random.random(self.size) * 0.4 - 0.2
         
     def gen_e(self, e_c, e_sigma):
         return np.random.normal(loc=e_c, scale=e_sigma, size = self.size)
@@ -55,23 +23,12 @@ class plutino:
         
     def gen_amp(self, amp_c, amp_max, amp_min):
         return np.random.triangular(amp_min, amp_c, amp_max, size = self.size) * np.pi / 180.
-    
-    def gen_phi(self, amp):
-        return np.pi + amp * np.sin(2*np.pi*np.random.random(self.size)) 
-        
-    def gen_M(self):
-        return 4*np.pi*np.random.random(self.size)
         
     def gen_node(self):
         return 2*np.pi*np.random.random(self.size) % (2*np.pi)
         
-    def gen_arg(self):
-        return (0.5*self.phi - 1.5*self.M - self.node + self.lambda_N) % (2*np.pi)
-        
-    def gen_H(self):
+    def gen_H(self, h0, h1):
         alpha = 0.9
-        h0 = 0
-        h1 = 10
         h0s10 = 10**(alpha*h0)
         h1s10 = 10**(alpha*h1)
         return np.log10(np.random.random(self.size)*(h1s10-h0s10) + h0s10) / alpha
@@ -120,11 +77,199 @@ class plutino:
         self.dec = np.arcsin(Z/(X**2+Y**2+Z**2)**0.5)
         self.ra = np.arctan2(Y, X) % (2*np.pi)
 
+
+class plutino(Resonant):
+    """
+    generate plutinos
+    units: au, radius
+    
+    """
+    def __init__(self, size = 1000, mjd = 57023, e_c = 0.175, e_sigma = 0.06, i_sigma = 12, amp_c = 75, amp_max = 155, amp_min = 0, h0 = 0, h1 = 9.5):
+        self.size = size
+        self.mjd = mjd
+        self.lambda_N = None
+        self.neptune_lambda()
+        self.mjd = mjd
+        self.a = self.gen_a()
+        self.e = self.gen_e(e_c, e_sigma)
+        self.i = self.gen_i(i_sigma)
+        self.amp = self.gen_amp(amp_c, amp_max, amp_min)
+        self.phi = self.gen_phi(self.amp)
+        self.M = self.gen_M()
+        self.node = self.gen_node()
+        self.arg = self.gen_arg()
+        self.H = self.gen_H(h0, h1)
+        cut = (self.e > 0) * (~np.isnan(self.i))
+        self.a = self.a[cut]
+        self.e = self.e[cut]
+        self.i = self.i[cut]
+        self.amp = self.amp[cut]
+        self.phi = self.phi[cut]
+        self.M = self.M[cut]
+        self.node = self.node[cut]
+        self.arg = self.arg[cut]
+        self.H = self.H[cut]
+        X, Y, Z, r = zip(*map(self.kep_to_xyz, self.a, self.e, self.i,\
+                                self.arg, self.node, self.M)) # r**2 = X**2 + Y**2 + Z**2
+        self.X = np.array(X)
+        self.Y = np.array(Y)
+        self.Z = np.array(Z)
+        self.r = np.array(r)
+        self.xyz_to_equa(self.X, self.Y, self.Z)
+        self.H_to_mag()
+        
+    
+    def gen_a(self):
+        return 39.45 + np.random.random(self.size) * 0.4 - 0.2
+        
+    def gen_phi(self, amp):
+        return np.pi + amp * np.sin(2*np.pi*np.random.random(self.size)) 
+        
+    def gen_M(self):
+        return 4*np.pi*np.random.random(self.size)
+        
+    def gen_arg(self):
+        return (0.5*self.phi - 1.5*self.M - self.node + self.lambda_N) % (2*np.pi)
+        
+
+class trojan(Resonant):
+    """
+    generate trojans
+    units: au, radius
+    
+    """
+    def __init__(self, size = 1000, mjd = 57023, e_c = 0.04, e_sigma =0.04, i_sigma = 12, amp_c = 10, amp_max = 25, amp_min = 5, h0 = 0, h1 = 10):
+        self.size = size
+        self.mjd = mjd
+        self.lambda_N = None
+        self.neptune_lambda()
+        self.a = self.gen_a()
+        self.e = self.gen_e(e_c, e_sigma)
+        self.i = self.gen_i(i_sigma)
+        self.amp = self.gen_amp(amp_c, amp_max, amp_min)
+        self.phi = self.gen_phi(self.amp)
+        self.M = self.gen_M()
+        self.node = self.gen_node()
+        self.arg = self.gen_arg()
+        self.H = self.gen_H(h0, h1)
+        cut = (self.e > 0) * (~np.isnan(self.i))
+        self.a = self.a[cut]
+        self.e = self.e[cut]
+        self.i = self.i[cut]
+        self.amp = self.amp[cut]
+        self.phi = self.phi[cut]
+        self.M = self.M[cut]
+        self.node = self.node[cut]
+        self.arg = self.arg[cut]
+        self.H = self.H[cut]
+        X, Y, Z, r = zip(*map(self.kep_to_xyz, self.a, self.e, self.i,\
+                                self.arg, self.node, self.M)) # r**2 = X**2 + Y**2 + Z**2
+        self.X = np.array(X)
+        self.Y = np.array(Y)
+        self.Z = np.array(Z)
+        self.r = np.array(r)
+        self.xyz_to_equa(self.X, self.Y, self.Z)
+        self.H_to_mag()
+        
+    
+    def gen_a(self):
+        return 30.025 + np.random.random(self.size) * 0.25 - 0.125
+            
+    def gen_phi(self, amp):
+        return ((1/3.*np.pi)+np.random.randint(-1, 1, self.size)*(2/3.*np.pi) + amp * np.sin(2*np.pi*np.random.random(self.size))) % (2*np.pi)    
+
+    def gen_M(self):
+        return 2*np.pi*np.random.random(self.size)
+        
+    def gen_arg(self):
+        return (self.phi - self.M - self.node + self.lambda_N) % (2*np.pi)
+
+class twotino(Resonant):
+    """
+    generate twotinos
+    units: au, radius
+    
+    """
+    def __init__(self, size = 1000, mjd = 57023, e_c = 0.2, e_sigma =0.06, i_sigma = 6, amp_c = 10, amp_max = 25, amp_min = 5, h0 = 0, h1 = 8):
+        self.size = size
+        self.mjd = mjd
+        self.lambda_N = None
+        self.neptune_lambda()
+        self.phi0 = None
+        self.gen_phi0()
+        self.a = self.gen_a()
+        self.e = self.gen_e(e_c, e_sigma)
+        self.i = self.gen_i(i_sigma)
+        self.amp = self.gen_amp()
+        self.phi = self.gen_phi()
+        self.M = self.gen_M()
+        self.node = self.gen_node()
+        self.arg = self.gen_arg()
+        self.H = self.gen_H(h0, h1)
+        cut = (~np.isnan(self.i))
+        self.a = self.a[cut]
+        self.e = self.e[cut]
+        self.i = self.i[cut]
+        self.amp = self.amp[cut]
+        self.phi = self.phi[cut]
+        self.M = self.M[cut]
+        self.node = self.node[cut]
+        self.arg = self.arg[cut]
+        self.H = self.H[cut]
+        X, Y, Z, r = zip(*map(self.kep_to_xyz, self.a, self.e, self.i,\
+                                self.arg, self.node, self.M)) # r**2 = X**2 + Y**2 + Z**2
+        self.X = np.array(X)
+        self.Y = np.array(Y)
+        self.Z = np.array(Z)
+        self.r = np.array(r)
+        self.xyz_to_equa(self.X, self.Y, self.Z)
+        self.H_to_mag()
+        
+    def gen_phi0(self):
+        self.phi0 = np.random.randint(-1, 2, self.size)
+        
+    #def gen_e(self):
+    #    return np.random.random(self.size) * 0.3 + 0.1 
+    
+    def gen_a(self):
+        return 47.8 + np.random.random(self.size) * 0.4 - 0.2
+            
+    def gen_phi(self):
+        phi = np.zeros(len(self.phi0))
+        sym = (self.phi0 == 0)
+        asym0 = (self.phi0 == -1)
+        asym1 = (self.phi0 == 1)
+        phi[sym] = np.pi + self.amp_sym * np.sin(2*np.pi*np.random.random(sym.sum())) % (2*np.pi)  
+        phi[asym0] = -90 * np.pi/180. + self.amp_0 * np.sin(2*np.pi*np.random.random(asym0.sum())) % (2*np.pi) 
+        phi[asym1] = 90 * np.pi/180. + self.amp_1 * np.sin(2*np.pi*np.random.random(asym1.sum())) % (2*np.pi) 
+        return phi 
+        
+    def gen_amp(self):
+        amp = np.zeros(len(self.phi0))
+        sym = (self.phi0 == 0)
+        asym0 = (self.phi0 == -1)
+        asym1 = (self.phi0 == 1)
+        self.amp_sym = (np.random.random(sym.sum())*15 + 135) * np.pi/180.
+        self.amp_0 = (np.random.random(asym0.sum())*45 + 15) * np.pi/180.
+        self.amp_1 = (np.random.random(asym1.sum())*45 + 15) * np.pi/180.
+        amp[sym] = self.amp_sym
+        amp[asym0] = self.amp_0
+        amp[asym1] = self.amp_1
+        return amp
+
+    def gen_M(self):
+        return 2*np.pi*np.random.random(self.size)
+        
+    def gen_arg(self):
+        return (self.phi - 2 * self.M - self.node + self.lambda_N) % (2*np.pi)
         
         
 def main():
-    p = plutino(size = 2000, e_c = 0.3, e_sigma = 0.01, amp_c = 1, amp_max = 2, amp_min = 0, i_sigma=12)
-    print p.ra[:100], p.dec[:100], p.mag[:100]
+    #p = plutino(size = 2000, e_c = 0.3, e_sigma = 0.01, amp_c = 1, amp_max = 2, amp_min = 0, i_sigma=12)
+    p = plutino(size = 2000000, mjd=57023)
+    #for i in zip(p.a, p.e, p.i, p.arg, p.node, p.M, p.H):
+    #    print i
+    print p.mag_g.min(), (p.mag_g < 23.5).sum()
     
 
 if __name__ == '__main__':
